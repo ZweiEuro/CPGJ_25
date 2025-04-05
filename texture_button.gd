d5561ea7ea10fcdd4ed95db69a5502c4fb9bbb02
Dominik Völkel <voelkel.dominik@gmail.com>
2025-04-05 14:16:28 +0200

    basic wav state files

--- texture_button.gd
+++ texture_button.gd
@@ -1,70 +1,86 @@
 extends TextureButton
 
 
-var turned_on = false;
 
+# textures
 @export_file("*.svg") var texture_on_path: String = "";
 var texture_on: Texture = null;
 
 @export_file("*.svg") var texture_off_path: String = "";
 var texture_off: Texture = null;
 
-@export_file("*.wav") var audio_on_path: String = "";
+# Audio 
 var audio_player: AudioStreamPlayer2D = null;
 
+@export_file("*.wav") var audio_windup_path: String = "";
+var audio_windup_stream: AudioStreamWAV = null;
+
+@export_file("*.wav") var audio_loop_path: String = "";
+var audio_loop_stream: AudioStreamWAV = null;
+
+@export_file("*.wav") var audio_winddown_path: String = "";
+var audio_winddown_stream: AudioStreamWAV = null;
+
+
+enum MachineState { start, loop, stop}
+var machine_state: MachineState  = MachineState.stop;
+
+func load_file_from_path(filePath:  String, ext: String, alternative_file = ""):
+	var ret = null
+	
+	if(filePath == ""):
+		if(alternative_file != ""):
+			ret = load_file_from_path(alternative_file, "svg");
+	else:
+		if(ext == "wav"):
+			ret = AudioStreamWAV.load_from_file(filePath);
+		if(ext == "svg"):
+			var image = Image.load_from_file(filePath)
+			ret = ImageTexture.create_from_image(image)
+	
+	return ret;
 
 func _ready():
-	if(texture_on_path == ""):
-		texture_on = preload("res://placeholder/icon_on.svg");
-	else:
-		var image = Image.load_from_file(texture_on_path)
-		texture_on = ImageTexture.create_from_image(image)
 	
-	if(texture_off_path == ""):
-		texture_off = preload("res://placeholder/icon_off.svg");
-	else:
-		var image = Image.load_from_file(texture_off_path)
-		texture_off = ImageTexture.create_from_image(image)
+	texture_on = load_file_from_path(texture_on_path, "svg", "res://placeholder/icon_on.svg")
+	texture_off = load_file_from_path(texture_off_path, "svg", "res://placeholder/icon_off.svg")
 	
 	audio_player = AudioStreamPlayer2D.new();
+	self.audio_player.finished.connect(self.on_audio_played)
+	self.add_child(audio_player);
 	
-	if(audio_on_path == ""):
-		audio_player.stream = AudioStreamWAV.load_from_file("res://placeholder/testsound.wav"); 
-	else:
-		audio_player.stream = AudioStreamWAV.load_from_file("res://placeholder/testsound.wav"); 
-		
-	self.add_child(audio_player)
+	print(audio_windup_path, audio_windup_path, audio_winddown_path)
 	
+	audio_windup_stream = load_file_from_path(audio_windup_path, "wav")
+	audio_loop_stream = load_file_from_path(audio_loop_path, "wav")
+	audio_winddown_stream = load_file_from_path(audio_winddown_path, "wav")
 	
-	texture_normal = texture_off
-		
-func select_texture():	
-	if(turned_on):
-		self.texture_normal = texture_on
-	else:
+	select_texture()
+
+func select_texture():
+	if(machine_state == MachineState.stop):
 		self.texture_normal = texture_off
+	else:
+		self.texture_normal = texture_on
 		
+
+# assigning already selected streams should not produce any overhead as they are 
+# already loaded
+func on_audio_played():
+	if(self.machine_state == MachineState.start):
+		self.audio_player.stream = audio_loop_stream;
+		self.audio_player.play()
+					
+				
 func _on_pressed() -> void:
-	print("turned: ", turned_on)
 	
-	if(turned_on):
-		turned_on = false;
-			
-	#Play off stream
-	# schedule looping stream
-			
-	audio_player.play();
+	self.audio_player.stop();
+	if( self.machine_state == MachineState.stop):
+		self.machine_state = MachineState.start
+		self.audio_player.stream = audio_windup_stream;
+	else:
+		self.audio_player.stream = audio_winddown_stream;
+		self.machine_state = MachineState.stop
+	self.audio_player.play()
 	select_texture()
-
-func start_entity():
-	print("Starting entity")
-	# play on stream
-	
-	audio_player.play()
-
-	turned_on = true;
-	select_texture()
-	
-
-	
 	
